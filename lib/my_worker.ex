0c4e7d26ba8c202f59f7854c4acc0f4073cf8907
David Salgado <david@digitalronin.com>
2015-12-12 15:33:58 +0000

    add start_link function, so MyWorker works with poolboy

--- lib/my_worker.ex
+++ lib/my_worker.ex
@@ -1,6 +1,10 @@
 defmodule MyWorker do
   use GenServer
 
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, nil, [])
+  end
+
   def handle_call(:do_work, _from, state) do
     IO.puts "process #{inspect self} doing work"
     :timer.sleep 1000
